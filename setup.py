a03418827e91b289cfd1cca054876d2a765110f3
i008 <kubacieslik@gmail.com>
2020-08-11 00:56:07 +0200

    1.6

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except ImportError:  # for pip < 10
     from pip.req import parse_requirements
 import os
 
-__version__ = '1.5'
+__version__ = '1.6'
 
 with open('README.MD') as readme_file:
     readme = readme_file.read()
