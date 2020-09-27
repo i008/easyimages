b029157c346ba6862b1ab235c77df93a5bbdbd2f
i008 <kubacieslik@gmail.com>
2020-09-28 00:54:40 +0200

    bump

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except ImportError:  # for pip < 10
     from pip.req import parse_requirements
 import os
 
-__version__ = '1.9'
+__version__ = '1.91'
 
 with open('README.MD') as readme_file:
     readme = readme_file.read()
